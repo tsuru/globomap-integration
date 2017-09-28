5a3a123e6344d4483508064023d0a3f8ddb23d98
Guilherme Garnier <guilherme.garnier@gmail.com>
2017-09-28 15:32:43 -0300

    Add test for event list with filters

--- tsuru_client_test.go
+++ tsuru_client_test.go
@@ -9,6 +9,7 @@ import (
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"gopkg.in/check.v1"
 )
@@ -48,6 +49,30 @@ func (s *S) TestEventList(c *check.C) {
 	c.Assert(events, check.HasLen, 2)
 	c.Assert(events[0].Target.Value, check.Equals, "myapp1")
 	c.Assert(events[1].Target.Value, check.Equals, "myapp2")
+}
+
+func (s *S) TestEventListWithFilters(c *check.C) {
+	until := time.Now()
+	since := until.Add(-1 * time.Hour)
+	server := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		c.Assert(r.FormValue("kindname"), check.Equals, "app.update")
+		c.Assert(r.FormValue("since"), check.Equals, since.Format(TIME_FORMAT))
+		c.Assert(r.FormValue("until"), check.Equals, until.Format(TIME_FORMAT))
+		w.WriteHeader(http.StatusNoContent)
+	}))
+	defer server.Close()
+	client := tsuruClient{
+		Hostname: server.URL,
+		Token:    s.token,
+	}
+
+	filter := eventFilter{
+		Kindname: "app.update",
+		Since:    &since,
+		Until:    &until,
+	}
+	_, err := client.EventList(filter)
+	c.Assert(err, check.IsNil)
 }
 
 func (s *S) TestEventListNoContent(c *check.C) {
